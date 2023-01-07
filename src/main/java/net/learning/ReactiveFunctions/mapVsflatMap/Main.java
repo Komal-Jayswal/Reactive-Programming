0d5bad013be4c0a89f99a1bdf4530ab4e456ed92
Komal-Jayswal <Komal.Jayswal@maersk.com>
2023-01-07 10:21:14 +0530

    Add MapVsFlatMap using flux

--- src/main/java/net/learning/ReactiveFunctions/mapVsflatMap/Main.java
+++ src/main/java/net/learning/ReactiveFunctions/mapVsflatMap/Main.java
@@ -1,12 +1,15 @@
 package net.learning.ReactiveFunctions.mapVsflatMap;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class MapVsFlatMap {
+public class Main {
 
     /**
      * Map in Funtional Programming
@@ -24,7 +27,7 @@ public class MapVsFlatMap {
     }
 
     /**
-     * Map in Reactive Programming
+     * Map in Reactive Programming with mono
      */
     public static void map_Reactive(){
 
@@ -32,9 +35,9 @@ public class MapVsFlatMap {
 
         Mono<List<String>> emails = customers //Mono<List<Customer>>
                 .map(customers1 // List<Customer>
-                        -> customers1.stream() // Stream<Customer>
-                        .map(customer //each customer Object
-                                -> customer.getEmail()) // Stream<Object> i.e Stream<String>
+                        -> //each customer Object
+                        customers1.stream() // Stream<Customer>
+                        .map(Customer::getEmail) // Stream<Object> i.e Stream<String>
                         .collect(Collectors.toList()) // List<Object> i.e List<String>
                 ); // Mono<Object> i.e Mono<List<String>>
 
@@ -57,13 +60,13 @@ public class MapVsFlatMap {
 
         List<List<String>> phoneNumbers = customers.  //List<Customer>
                 stream() //Stream<Customer>
-                .map(customer -> customer.getPhoneNumbers()) //Stream<List<String>>
-                .collect(Collectors.toList()); //List<List<String>>
+                .map(customer -> customer.getPhoneNumbers())
+                .toList(); //List<List<String>>
 
         List<String> phones = customers //List<Customer>
                 .stream() //Stream<Customer>
-                .flatMap(customer -> customer.getPhoneNumbers().stream()) //Stream<String>
-                .collect(Collectors.toList()); //List<String>
+                .flatMap(customer -> customer.getPhoneNumbers().stream())
+                .toList(); //List<String>
 
         System.out.println("\n Functional Map vs Flatmap : \n");
         System.out.println("Map : "+ phoneNumbers);
@@ -72,7 +75,7 @@ public class MapVsFlatMap {
     }
 
     /**
-     * Map Vs FlatMap in Reactive Programming
+     * Map Vs FlatMap in Reactive Programming with mono
      */
     public static void mapVsFlatMap_Reactive() {
 
@@ -105,11 +108,64 @@ public class MapVsFlatMap {
                 .subscribe(System.out::println);
     }
 
+    /**
+     * Map Vs FlatMap in Reactive Programming with flux
+     */
+    public static void mapVsFlatMapWithFluxInReactive() {
+
+        List<Customer> customerList = new ArrayList<>();
+
+        customerList.add(new Customer(101, "john", "john@gmail.com", Arrays.asList("397937955", "21654725")));
+        customerList.add(new Customer(102, "smith", "smith@gmail.com", Arrays.asList("89563865", "2487238947")));
+        customerList.add(new Customer(103, "peter", "peter@gmail.com", Arrays.asList("38946328654", "3286487236")));
+        customerList.add(new Customer(104, "kely", "kely@gmail.com", Arrays.asList("389246829364", "948609467")));
+
+        Flux<Customer> customers = Flux.fromIterable(customerList);
+
+        customers.doFirst(()-> System.out.println("\n Customers Details From Database in form of flux \n"))
+                .subscribe(System.out::println);
+
+        Mono<List<String>> emailMono = customers
+                .map(Customer::getEmail)
+                .collect(Collectors.toList());
+
+        emailMono.doFirst(()-> System.out.println("\n\n Map : Collect all customer's email in form of Mono<List<String>> \n"))
+                .subscribe(System.out::println);
+
+        Flux<String> emailFlux = customers
+                .map(Customer::getEmail);
+
+        emailFlux.doFirst(()-> System.out.println("\n\n Map : Collect all customer's email in form of Flux<String> \n"))
+                .subscribe(System.out::println);
+
+        Mono<List<List<String>>> phoneMono = customers
+                .map(Customer::getPhoneNumbers)
+                .collect(Collectors.toList());
+
+        phoneMono.doFirst(()-> System.out.println("\n\n Map : Collect all customer's Phone Numbers in form of Mono<List<List<String>>> \n"))
+                .subscribe(System.out::println);
+
+        Flux<List<String>> phoneFlux = customers
+                .map(Customer::getPhoneNumbers);
+
+        phoneFlux.doFirst(()-> System.out.println("\n\n Map : Collect all customer's Phone Numbers in form of Flux<List<String>> \n"))
+                .subscribe(System.out::println);
+
+        Flux<String> phoneFlux_FlatMap = Flux.fromIterable(customers
+                .toStream() //Stream<customer>
+                .flatMap(customer -> customer.getPhoneNumbers().stream()) //Stream<String>
+                .collect(Collectors.toList())); //Flux<String>
+
+        phoneFlux_FlatMap.doFirst(()-> System.out.println("\n\n FlatMap : Collect all customer's Phone Numbers in form of Flux<String> \n"))
+                .subscribe(System.out::println);
+    }
+
     public static void divider() {
         IntStream.range(1,200).forEach(c-> System.out.print("-"));
     }
 
     public static void main(String[] args) {
+
         map_Reactive();
         divider();
         map_Functional();
@@ -117,6 +173,8 @@ public class MapVsFlatMap {
         mapVsFlatMap_Functional();
         divider();
         mapVsFlatMap_Reactive();
+        divider();
+        mapVsFlatMapWithFluxInReactive();
 
     }
 }
